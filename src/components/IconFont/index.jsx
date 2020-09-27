6e029b791eb5a3d33c3794bb6da6e6538a9f8d45
jacqui126 <15617938928@163.com>
2020-09-27 16:49:16 +0800

    use iconfont

---
+++ src/components/IconFont/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { createFromIconfontCN } from '@ant-design/icons';
+
+
+export const IconFont = createFromIconfontCN({
+    scriptUrl: '//at.alicdn.com/t/font_2103771_ka1xg8nab08.js',
+});
